de9bb392aa6e3afb8b9b827a3f07e6b6fc6d125e
Karl Wallenius <karl.wallenius@gmail.com>
2023-10-01 16:25:43 +0200

    feat: add functions and methods for handling triggers

--- triggers.go
+++ triggers.go
@@ -4,9 +4,39 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
 )
+
+var (
+	// ErrNotHTTPTrigger is returned when the provide trigger is not
+	// an HTTP trigger.
+	ErrNotHTTPTrigger = errors.New("not an HTTP trigger")
+	// ErrTriggerNameIncorrect is returned when the provided trigger
+	// name does not match the payload trigger name.
+	ErrTriggerNameIncorrect = errors.New("trigger name incorrect")
+	// ErrTriggerPayloadMalformed is returned if there is an error
+	// with the payload from the Function host.
+	ErrTriggerPayloadMalformed = errors.New("trigger payload malformed")
+)
+
+// RawMessage is a type based on json.RawMessage with a custom UnmarshalJSON
+// method to handle escaped JSON.
+type RawMessage json.RawMessage
+
+// UnmarshalJSON satisfies json.Unmarshaler and unquotes the incoming
+// data. If it cannot be unquoted it's assumed it is not unquoted.
+func (r *RawMessage) UnmarshalJSON(b []byte) error {
+	unquoted, err := strconv.Unquote(string(b))
+	if err == nil {
+		*r = RawMessage(unquoted)
+		return nil
+	}
+	*r = RawMessage(b)
+	return nil
+}
 
 // trigger is the interface that wraps around method Data.
 type trigger interface {
@@ -16,7 +46,7 @@ type trigger interface {
 // Trigger represents an incoming request (trigger) from the
 // Azure Function Host.
 type Trigger[T trigger] struct {
-	Data     map[string]T
+	Payload  map[string]T `json:"Data"`
 	Metadata map[string]any
 	d        []byte
 	n        string
@@ -28,12 +58,22 @@ func (t Trigger[T]) Parse(v any) error {
 	return json.Unmarshal(t.d, &v)
 }
 
+// Data returns the data contained in the trigger.
+func (t Trigger[T]) Data() []byte {
+	return t.d
+}
+
+// Trigger returns underlying trigger.
+func (t Trigger[T]) Trigger() T {
+	return t.Payload[t.n]
+}
+
 // HTTPTrigger represnts a Function App HTTP Trigger and contains
 // the incoming HTTP data.
 type HTTPTrigger struct {
 	URL     string `json:"Url"`
 	Method  string
-	Body    json.RawMessage
+	Body    RawMessage
 	Headers http.Header
 	Params  map[string]string
 	Query   map[string]string
@@ -47,7 +87,7 @@ func (t HTTPTrigger) Data() []byte {
 // GenericTrigger represents a generic Function App trigger. With custom handlers all
 // triggers that are not HTTP triggers share the same data structure.
 type GenericTrigger struct {
-	json.RawMessage
+	RawMessage
 }
 
 // Data returns data of the trigger as a []byte.
@@ -55,86 +95,128 @@ func (t GenericTrigger) Data() []byte {
 	return t.RawMessage
 }
 
-// TriggerOption[T] is function that sets option on a Trigger[T]
-type TriggerOption[T trigger] func(*Trigger[T])
+// TriggerOptions contains options for functions and methods related
+// to triggers.
+type TriggerOptions struct {
+	Name string
+}
+
+// TriggerOption[T] is function that sets options on TriggerOptions.
+type TriggerOption func(*TriggerOptions)
 
 // WithName sets the trigger name get data from. The name should
 // match the incoming trigger (binding) name in function.json.
-func WithName[T trigger](name string) TriggerOption[T] {
-	return func(t *Trigger[T]) {
-		if _, ok := t.Data[name]; ok {
-
-		}
+func WithName(name string) TriggerOption {
+	return func(o *TriggerOptions) {
+		o.Name = name
 	}
 }
 
 // NewTrigger handles a request from the Function host and returns a Trigger[T].
-func NewTrigger[T trigger](r *http.Request, options ...TriggerOption[T]) (Trigger[T], error) {
-	t := Trigger[T]{
-		n: "req",
-	}
+func NewTrigger[T trigger](r *http.Request, options ...TriggerOption) (Trigger[T], error) {
+	opts := TriggerOptions{}
 	for _, option := range options {
-		option(&t)
+		option(&opts)
+	}
+	if len(opts.Name) == 0 {
+		opts.Name = "req"
 	}
 
+	t := Trigger[T]{
+		n: opts.Name,
+	}
 	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
-		return Trigger[T]{}, err
+		return Trigger[T]{}, fmt.Errorf("%w: %w", ErrTriggerPayloadMalformed, err)
 	}
 
-	d, ok := t.Data[t.n]
+	d, ok := t.Payload[t.n]
 	if !ok {
-		return Trigger[T]{}, errors.New("name incorrect")
+		return Trigger[T]{}, ErrTriggerNameIncorrect
 	}
 	t.d = d.Data()
 	return t, nil
 }
 
-// QueueTrigger represnts a Function App Queue Trigger and contains
-// the incoming queue message data.
-type QueueTrigger = GenericTrigger
-
-// RequestFrom takes the request from the Function Host and creates
+// NewRequest takes the request from the Function Host and creates
 // a new *http.Request from it. Suitable in scenarios like a middleware
 // to extract data from an HTTP trigger request (such as headers etc),
 // or pass it on to the next handler as an ordinarily formatted
 // *http.Request.
-func RequestFrom(r *http.Request) (*http.Request, error) {
-	input, err := NewTrigger[HTTPTrigger](r)
+func NewRequest(r *http.Request) (*http.Request, error) {
+	trigger, err := NewTrigger[HTTPTrigger](r)
 	if err != nil {
 		return nil, err
 	}
-
-	// If "req" exists, it's an HTTP trigger.
-	data, ok := input.Data["req"]
+	request, ok := trigger.Payload["req"]
 	if !ok {
-		return nil, errors.New("not an HTTP trigger")
+		return nil, ErrNotHTTPTrigger
 	}
 
-	u, err := url.Parse(data.URL)
+	u, err := buildURL(request.URL, request.Params, request.Query)
 	if err != nil {
 		return nil, err
 	}
-	for k, v := range data.Params {
-		u.Path, err = url.JoinPath(u.Path, k+"/"+v)
-		if err != nil {
-			return nil, err
-		}
-	}
-	for k, v := range data.Query {
-		u.Query().Add(k, v)
-	}
-	u.RawQuery = u.Query().Encode()
 
 	var body *bytes.Buffer
-	if data.Body != nil {
-		body = bytes.NewBuffer(data.Body)
+	if request.Body != nil {
+		body = bytes.NewBuffer(request.Body)
 	}
 
-	req, err := http.NewRequest(data.Method, u.String(), body)
+	req, err := http.NewRequest(request.Method, u, body)
 	if err != nil {
 		return nil, err
 	}
-	req.Header = data.Headers
+	req.Header = request.Headers
 
 	return req, nil
 }
+
+// Parse the incoming Function host request (trigger) and set
+// the data to the provided value.
+func Parse[T trigger](r *http.Request, v any, options ...TriggerOption) error {
+	trigger, err := NewTrigger[T](r, options...)
+	if err != nil {
+		return err
+	}
+	return trigger.Parse(v)
+}
+
+// Data returns the data from the incoming Function host
+// request (trigger).
+func Data[T trigger](r *http.Request, options ...TriggerOption) ([]byte, error) {
+	trigger, err := NewTrigger[T](r, options...)
+	if err != nil {
+		return nil, err
+	}
+	return trigger.Data(), nil
+}
+
+// buildURL from the provided url, parameters and query.
+func buildURL(u string, p, q map[string]string) (string, error) {
+	_url, err := url.Parse(u)
+	if err != nil {
+		return "", err
+	}
+	for k, v := range p {
+		_url.Path, err = url.JoinPath(_url.Path, k+"/"+v)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	if q != nil {
+		query := _url.Query()
+		for k, v := range q {
+			query.Add(k, v)
+		}
+		_url.RawQuery = query.Encode()
+	}
+
+	return _url.String(), nil
+}
+
+// Trigger aliases.
+
+// QueueTrigger represnts a Function App Queue Trigger and contains
+// the incoming queue message data.
+type QueueTrigger = GenericTrigger
