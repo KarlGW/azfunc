93b5a0bab0229847f1935ca874e701238890a6e8
Karl Wallenius <karl.wallenius@gmail.com>
2023-10-19 22:57:36 +0200

    feat: add Timer to triggers

---
+++ triggers/timer.go
@@ -0,0 +1,79 @@
+package triggers
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/KarlGW/azfunc/data"
+)
+
+// Timer represent a Timer trigger.
+type Timer struct {
+	Schedule       TimerSchedule
+	ScheduleStatus TimerScheduleStatus
+	IsPastDue      bool
+	Metadata
+}
+
+// TimerSchedule represents the Schedule field from the incoming
+// request.
+type TimerSchedule struct {
+	AdjustForDST bool
+}
+
+// TimerScheduleStatus represents the ScheduleStatus field from
+// the incoming request.
+type TimerScheduleStatus struct {
+	Last        time.Time
+	Next        time.Time
+	LastUpdated time.Time
+}
+
+// Parse together with Data satisfies the triggerable interface. It
+// is a no-op method. A timer trigger contains no other data needed
+// to be parsed. Use the fields directly.
+func (t Timer) Parse(v any) error {
+	return nil
+}
+
+// Data together with Parse satisfies the triggerable interface. It
+// is a no-op method. A timer trigger contains no other data needed
+// to be parsed. Use the fields directly.
+func (t Timer) Data() data.Raw {
+	return nil
+}
+
+// NewTimer creates and returns a Timer trigger from the provided
+// *http.Request.
+func NewTimer(r *http.Request, options ...Option) (*Timer, error) {
+	opts := Options{}
+	for _, option := range options {
+		option(&opts)
+	}
+
+	var t timerTrigger
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		return nil, ErrTriggerPayloadMalformed
+	}
+	defer r.Body.Close()
+
+	return &Timer{
+		Schedule:       t.Data.Timer.Schedule,
+		ScheduleStatus: t.Data.Timer.ScheduleStatus,
+		IsPastDue:      t.Data.Timer.IsPastDue,
+		Metadata:       t.Metadata,
+	}, nil
+}
+
+// timerTrigger is the incoming request from the Function host.
+type timerTrigger struct {
+	Data struct {
+		Timer struct {
+			Schedule       TimerSchedule
+			ScheduleStatus TimerScheduleStatus
+			IsPastDue      bool
+		} `json:"timer"`
+	}
+	Metadata Metadata
+}
---
+++ triggers/timer_test.go
@@ -0,0 +1,91 @@
+package triggers
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNewTimer(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input struct {
+			req     *http.Request
+			options []Option
+		}
+		want    *Timer
+		wantErr error
+	}{
+		{
+			name: "NewTimer",
+			input: struct {
+				req     *http.Request
+				options []Option
+			}{
+				req: &http.Request{
+					Body: io.NopCloser(bytes.NewBuffer(timerRequest1)),
+				},
+			},
+			want: &Timer{
+				Schedule: TimerSchedule{
+					AdjustForDST: true,
+				},
+				ScheduleStatus: TimerScheduleStatus{
+					Last:        _testTime1,
+					Next:        _testTime1,
+					LastUpdated: _testTime1,
+				},
+				IsPastDue: false,
+				Metadata: Metadata{
+					Sys: MetadataSys{
+						MethodName: "helloTimer",
+						UtcNow:     _testTime1,
+						RandGuid:   "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+					},
+				},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, gotErr := NewTimer(test.input.req)
+
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("NewTimer() = unexpected result (-want +got)\n%s\n", diff)
+			}
+
+			if diff := cmp.Diff(test.wantErr, gotErr); diff != "" {
+				t.Errorf("NewTimer() = unexpected error (-want +got)\n%s\n", diff)
+			}
+		})
+	}
+}
+
+var timerRequest1 = []byte(`{
+	"Data": {
+	  "timer": {
+		"Schedule": {
+		  "AdjustForDST": true
+		},
+		"ScheduleStatus": {
+		  "Last": "2023-10-12T20:13:49.640002Z",
+		  "Next": "2023-10-12T20:13:49.640002Z",
+		  "LastUpdated": "2023-10-12T20:13:49.640002Z"
+		},
+		"IsPastDue": false
+	  }
+	},
+	"Metadata": {
+	  "sys": {
+		"MethodName": "helloTimer",
+		"UtcNow": "2023-10-12T20:13:49.640002Z",
+		"RandGuid": "4e773554-f6b7-4ea2-b07d-4c5fd5aba741"
+	  }
+	}
+  }
+`)
