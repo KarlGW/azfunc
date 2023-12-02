2a1c92b4e011f320121c1e6fce344b2ef09ee943
Karl <karl.wallenius@gmail.com>
2023-12-02 23:11:08 +0100

    feat: add more methods to logger interface (update noOpLogger) (#17)

--- function_app.go
+++ function_app.go
@@ -22,11 +22,13 @@ var (
 	ErrInvalidTrigger = errors.New("invalid trigger")
 )
 
-// logger is the interface that wraps around methods Info
-// and Error.
+// logger is the interface that wraps around methods Debug, Error, Info
+// and Warn.
 type logger interface {
-	Info(msg string, args ...any)
+	Debug(msg string, args ...any)
 	Error(msg string, args ...any)
+	Info(msg string, args ...any)
+	Warn(msg string, args ...any)
 }
 
 // FunctionApp represents a Function App with its configuration
@@ -223,8 +225,14 @@ func WithLogger(log logger) FunctionAppOption {
 // function app.
 type noOpLogger struct{}
 
-// Info together with Error satisfies the logger interface.
+// Debug together with Error, Info and Warn satisfies the logger interface.
+func (l noOpLogger) Debug(msg string, args ...any) {}
+
+// Error together with Debug, Info and Warn satisfies the logger interface.
+func (l noOpLogger) Error(msg string, args ...any) {}
+
+// Info together with Debug, Error and Warn satisfies the logger interface.
 func (l noOpLogger) Info(msg string, args ...any) {}
 
-// Error together with Info satisfies the logger interface.
-func (l noOpLogger) Error(msg string, args ...any) {}
+// Warn together with Debug, Info and Error satisfies the logger interface.
+func (l noOpLogger) Warn(msg string, args ...any) {}
