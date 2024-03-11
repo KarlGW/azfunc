c5fb511a3d42f9212bb9b4cc455464cf06b5264f
Karl Wallenius <karl.wallenius@gmail.com>
2024-03-11 22:28:02 +0100

    docs: update doc comments for bindable

--- bindings/bindings.go
+++ bindings/bindings.go
@@ -2,7 +2,7 @@ package bindings
 
 import "github.com/KarlGW/azfunc/data"
 
-// Bindable is the interface that wraps around methods Name and Write.
+// Bindable is the interface that wraps around methods Data, Name and Write.
 type Bindable interface {
 	// Data returns the data of the binding.
 	Data() data.Raw
