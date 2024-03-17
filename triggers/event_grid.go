3d26968fadac7b5ed219cd37d36022d5b4fff407
Karl Wallenius <karl.wallenius@gmail.com>
2024-03-17 12:16:57 +0100

    feat: add event grid trigger

---
+++ triggers/event_grid.go
@@ -0,0 +1,127 @@
+package triggers
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/KarlGW/azfunc/data"
+)
+
+// EventGridSchema is the type of schema that the Event Grid trigger
+// is using.
+type EventGridSchema int
+
+const (
+	// EventGridSchemaCloudEvents is the CloudEvents schema.
+	EventGridSchemaCloudEvents EventGridSchema = iota
+	// EventGridSchemaEventGrid is the Event Grid schema.
+	EventGridSchemaEventGrid
+)
+
+// EventGrid represents an Event Grid trigger. It handles both
+// Cloud Events and Event Grid events. Cloud events is the recommended
+// schema to use, and thus those property names are used.
+// The Even Grid schema properties 'Topic', 'EventType' and 'EventTime'
+// is set to the properties 'Source', 'Type' and 'Time' respectively.
+type EventGrid struct {
+	Time     time.Time
+	Metadata EventGridMetadata
+	ID       string
+	Source   string
+	Subject  string
+	Type     string
+	Data     data.Raw
+	Schema   EventGridSchema
+}
+
+// EventGridOptions contains options for an Event Grid trigger.
+type EventGridOptions struct {
+	Schema EventGridSchema
+}
+
+// EventGridOption is a function that sets options on an Event Grid
+// trigger.
+type EventGridOption func(o *EventGridOptions)
+
+// EventGridMetadata represents the metadata for an Event Grid trigger.
+type EventGridMetadata struct {
+	Metadata
+	Data data.Raw `json:"data"`
+}
+
+// Parse the data from the Event Grid trigger into the provided value.
+func (t EventGrid) Parse(v any) error {
+	return json.Unmarshal(t.Data, &v)
+}
+
+// NewEventGrid creates and returns an Event Grid trigger from the provided
+// *http.Request. The
+func NewEventGrid(r *http.Request, name string, options ...EventGridOption) (*EventGrid, error) {
+	opts := EventGridOptions{
+		Schema: EventGridSchemaCloudEvents,
+	}
+	for _, option := range options {
+		option(&opts)
+	}
+
+	var t eventGridTrigger
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		return nil, ErrTriggerPayloadMalformed
+	}
+
+	d, ok := t.Data[name]
+	if !ok {
+		return nil, ErrTriggerNameIncorrect
+	}
+
+	var source, typ string
+	var eventTime time.Time
+	var schema EventGridSchema
+	if len(d.SpecVersion) > 0 {
+		source, typ = d.Source, d.Type
+		eventTime = d.Time
+		schema = EventGridSchemaCloudEvents
+	} else if len(d.MetadataVersion) > 0 {
+		source, typ = d.Topic, d.EventType
+		eventTime = d.EventTime
+		schema = EventGridSchemaEventGrid
+	} else {
+		return nil, ErrTriggerPayloadMalformed
+	}
+
+	return &EventGrid{
+		ID:       d.ID,
+		Source:   source,
+		Subject:  d.Subject,
+		Type:     typ,
+		Time:     eventTime,
+		Data:     d.Data,
+		Metadata: t.Metadata,
+		Schema:   schema,
+	}, nil
+}
+
+// eventGridTrigger is the incoming request from the Function host.
+type eventGridTrigger struct {
+	Data     map[string]event
+	Metadata EventGridMetadata
+}
+
+// event is the incoming event from the Function host. It contains
+// all the properties that are included in both the cloud events
+// schema and the event grid schema.
+type event struct {
+	ID              string    `json:"id"`
+	Source          string    `json:"source"`
+	Topic           string    `json:"topic"`
+	Subject         string    `json:"subject"`
+	Type            string    `json:"type"`
+	EventType       string    `json:"eventType"`
+	Time            time.Time `json:"time"`
+	EventTime       time.Time `json:"eventTime"`
+	SpecVersion     string    `json:"specversion"`
+	DataVersion     string    `json:"dataVersion"`
+	MetadataVersion string    `json:"metadataVersion"`
+	Data            data.Raw  `json:"data"`
+}
---
+++ triggers/event_grid_test.go
@@ -0,0 +1,256 @@
+package triggers
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/KarlGW/azfunc/data"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNewEventGrid(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input struct {
+			req     *http.Request
+			name    string
+			options []EventGridOption
+		}
+		want    *EventGrid
+		wantErr error
+	}{
+		{
+			name: "NewEventGrid - cloud event",
+			input: struct {
+				req     *http.Request
+				name    string
+				options []EventGridOption
+			}{
+				req: &http.Request{
+					Body: io.NopCloser(bytes.NewBuffer(eventGridCloudEventRequest1)),
+				},
+				name: "event",
+			},
+			want: &EventGrid{
+				ID:      "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+				Source:  "source",
+				Subject: "subject",
+				Type:    "created",
+				Time:    _testEventGridTime1,
+				Data:    data.Raw(`{"id":"4e773554-f6b7-4ea2-b07d-4c5fd5aba741","name":"test"}`),
+				Schema:  EventGridSchemaCloudEvents,
+				Metadata: EventGridMetadata{
+					Data: data.Raw(`{"id":"4e773554-f6b7-4ea2-b07d-4c5fd5aba741","name":"test"}`),
+					Metadata: Metadata{
+						Sys: MetadataSys{
+							MethodName: "testevent",
+							UTCNow:     _testEventGridTime1,
+							RandGuid:   "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "NewEventGrid - event grid",
+			input: struct {
+				req     *http.Request
+				name    string
+				options []EventGridOption
+			}{
+				req: &http.Request{
+					Body: io.NopCloser(bytes.NewBuffer(eventGridEventRequest1)),
+				},
+				name: "event",
+			},
+			want: &EventGrid{
+				ID:      "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+				Source:  "source",
+				Subject: "subject",
+				Type:    "created",
+				Time:    _testEventGridTime1,
+				Data:    data.Raw(`{"id":"4e773554-f6b7-4ea2-b07d-4c5fd5aba741","name":"test"}`),
+				Schema:  EventGridSchemaEventGrid,
+				Metadata: EventGridMetadata{
+					Data: data.Raw(`{"id":"4e773554-f6b7-4ea2-b07d-4c5fd5aba741","name":"test"}`),
+					Metadata: Metadata{
+						Sys: MetadataSys{
+							MethodName: "testevent",
+							UTCNow:     _testEventGridTime1,
+							RandGuid:   "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, gotErr := NewEventGrid(test.input.req, test.input.name, test.input.options...)
+
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("NewEventGrid() = unexpected result (-want +got)\n%s\n", diff)
+			}
+
+			if diff := cmp.Diff(test.wantErr, gotErr); diff != "" {
+				t.Errorf("NewEventGrid() = unexpected error (-want +got)\n%s\n", diff)
+			}
+		})
+	}
+}
+
+func TestEventGrid_Parse(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input struct {
+			req     *http.Request
+			name    string
+			options []EventGridOption
+		}
+		want    eventGridTest
+		wantErr error
+	}{
+		{
+			name: "Parse",
+			input: struct {
+				req     *http.Request
+				name    string
+				options []EventGridOption
+			}{
+				req: &http.Request{
+					Body: io.NopCloser(bytes.NewBuffer(eventGridCloudEventRequest1)),
+				},
+				name: "event",
+			},
+			want: eventGridTest{
+				ID:   "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+				Name: "test",
+			},
+		},
+		{
+			name: "Parse - pretty JSON",
+			input: struct {
+				req     *http.Request
+				name    string
+				options []EventGridOption
+			}{
+				req: &http.Request{
+					Body: io.NopCloser(bytes.NewBuffer(eventGridCloudEventRequest2)),
+				},
+				name: "event",
+			},
+			want: eventGridTest{
+				ID:   "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+				Name: "test",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			trigger, err := NewEventGrid(test.input.req, test.input.name, test.input.options...)
+			if err != nil {
+				t.Fatalf("NewEventGrid() = unexpected error: %v", err)
+			}
+
+			var got eventGridTest
+			gotErr := trigger.Parse(&got)
+
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("Parse() = unexpected result (-want +got)\n%s\n", diff)
+			}
+
+			if test.wantErr == nil && gotErr != nil {
+				t.Errorf("Parse() = unexpected error: %v\n", gotErr)
+			}
+
+		})
+	}
+}
+
+var eventGridCloudEventRequest1 = []byte(`{
+	"Data": {
+	  "event": {
+		"id": "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+		"source": "source",
+		"specversion": "1.0",
+		"type": "created",
+		"subject": "subject",
+		"time": "2023-10-12T20:13:49.640002Z",
+		"data": {"id":"4e773554-f6b7-4ea2-b07d-4c5fd5aba741","name":"test"}
+	  }
+	},
+	"Metadata": {
+	  "data": {"id":"4e773554-f6b7-4ea2-b07d-4c5fd5aba741","name":"test"},
+	  "sys": {
+		"MethodName": "testevent",
+		"UtcNow": "2023-10-12T20:13:49.640002Z",
+		"RandGuid": "4e773554-f6b7-4ea2-b07d-4c5fd5aba741"
+	  }
+	}
+  }
+`)
+
+var eventGridCloudEventRequest2 = []byte(`{
+	"Data": {
+	  "event": {
+		"id": "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+		"source": "source",
+		"specversion": "1.0",
+		"type": "created",
+		"subject": "subject",
+		"time": "2023-10-12T20:13:49.640002Z",
+		"data": {
+		  "id": "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+		  "name": "test"
+		}
+	  }
+	},
+	"Metadata": {
+	  "data": {
+		"id": "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+		"name": "test"
+	  },
+	  "sys": {
+		"MethodName": "testevent",
+		"UtcNow": "2023-10-12T20:13:49.640002Z",
+		"RandGuid": "4e773554-f6b7-4ea2-b07d-4c5fd5aba741"
+	  }
+	}
+  }
+`)
+
+var eventGridEventRequest1 = []byte(`{
+	"Data": {
+	  "event": {
+		"id": "4e773554-f6b7-4ea2-b07d-4c5fd5aba741",
+		"topic": "source",
+		"subject": "subject",
+		"eventType": "created",
+		"dataVersion": "1",
+		"metadataVersion": "1",
+		"eventTime": "2023-10-12T20:13:49.640002Z",
+		"data": {"id":"4e773554-f6b7-4ea2-b07d-4c5fd5aba741","name":"test"}
+	  }
+	},
+	"Metadata": {
+	  "data": {"id":"4e773554-f6b7-4ea2-b07d-4c5fd5aba741","name":"test"},
+	  "sys": {
+		"MethodName": "testevent",
+		"UtcNow": "2023-10-12T20:13:49.640002Z",
+		"RandGuid": "4e773554-f6b7-4ea2-b07d-4c5fd5aba741"
+	  }
+	}
+  }
+`)
+
+var _testEventGridTime1, _ = time.Parse("2006-01-02T15:04:05.999999Z", "2023-10-12T20:13:49.640002Z")
+
+type eventGridTest struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
